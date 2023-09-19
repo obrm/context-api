bb14e0fe042a62345f06515ceca8c9732dc9c354
Ori Baram <obrm770@gmail.com>
2023-09-19 13:04:54 +0300

    first commit

---
+++ src/context/context.jsx
@@ -0,0 +1,50 @@
+import { useState, createContext } from 'react';
+
+import subLinks from '../data';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = (text, coordinates) => {
+    const page = subLinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppProvider, AppContext };
---
+++ src/context/useGlobalContext.js
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+
+import { AppContext } from './context';
+
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
